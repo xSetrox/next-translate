a41ebd428445bb7877905a7b1fe6d5bd10c147c2
Lance <lance.faltinsky@gmail.com>
2022-09-12 16:13:20 -0400

    initial

---
+++ next_translate.lua
@@ -0,0 +1,71 @@
+local language_codes_by_enum = {
+    [0]= "en-us",
+    [1]= "fr-fr",
+    [2]= "de-de",
+    [3]= "it-it",
+    [4]= "es-es",
+    [5]= "pt-br",
+    [6]= "pl-pl",
+    [7]= "ru-ru",
+    [8]= "ko-kr",
+    [9]= "zh-tw",
+    [10] = "ja-jp",
+    [11] = "es-mx",
+    [12] = "zh-cn"
+}
+
+local my_lang = lang.get_current()
+
+function encode_for_web(text)
+	return string.gsub(text, "%s", "+")
+end
+
+
+function get_iso_version_of_lang(lang_code)
+    lang_code = string.lower(lang_code)
+    if lang_code ~= "zh-cn" and lang_code ~= "zh-tw" then
+        return string.split(lang_code, '-')[1]
+    else
+        return lang_code
+    end
+end
+
+local iso_my_lang = get_iso_version_of_lang(my_lang)
+
+local do_translate = false
+menu.toggle(menu.my_root(), "On [BETA]", {"nextton"}, "Turns translating on/off. THIS IS A BETA SCRIPT!", function(on)
+    do_translate = on
+end, false)
+
+local only_translate_foreign = true
+menu.toggle(menu.my_root(), "Only translate foreign game lang", {"nextforeignonly"}, "Only translates messages from users with a different game language, thus saving API calls. You should leave this on to prevent the chance of Google temporarily blocking your requests.", function(on)
+    only_translate_foreign = on
+end, true)
+
+chat.on_message(function(sender, reserved, text, team_chat, networked, is_auto)
+    if do_translate and networked then
+        local encoded_text = encode_for_web(text)
+        local player_lang = language_codes_by_enum[players.get_language(sender)]
+        local player_name = players.get_name(sender)
+        if only_translate_foreign and player_lang == my_lang then
+            return
+        end
+        -- credit to the original chat translator for the api code
+        local translation
+        async_http.init("translate.googleapis.com", "/translate_a/single?client=gtx&sl=auto&tl=" .. iso_my_lang .."&dt=t&q=".. encoded_text, function(data)
+			translation, original, source_lang = data:match("^%[%[%[\"(.-)\",\"(.-)\",.-,.-,.-]],.-,\"(.-)\"")
+            if source_lang == nil then 
+                util.toast("Failed to translate a message from " .. player_name)
+                return
+            end
+            if get_iso_version_of_lang(source_lang) ~= iso_my_lang then
+                chat.send_message(string.gsub(player_name .. ': \"' .. translation .. '\"', "%+", " "), team_chat, true, false)
+            end
+		end, function()
+            util.toast("Failed to translate a message from " .. player_name)
+        end)
+		async_http.dispatch()
+    end
+end)
+
+util.keep_running()
